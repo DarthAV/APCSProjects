6cbd4efdb2ff57c5de71be1ddacd9943cc132bfb
DarthAV <Ansh.Verma22@outlook.com>
2020-05-16 11:22:31 -0700

    Allows a premade board to be used

--- Boggle/src/Board.java
+++ Boggle/src/Board.java
@@ -4,94 +4,114 @@
  * 
  */
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Board {
 	private char[][] board;
 	private WordList allWordList;
-	
-	public Board(WordList wordList, int size) {
+
+	public Board(WordList wordList, int size) throws FileNotFoundException {
 		this.board = new char[size][size];
 		this.allWordList = wordList;
-		
-		
-	    //use the created string to generate the board's characters randomly
+		System.out.println(allWordList.size());
+		populateBoard();
+	}
+
+	public void populateBoard() throws FileNotFoundException {
+
+		Scanner scanner = new Scanner(new File("Board.txt"));
 		for (int i = 0; i < board.length; i++) {
+			Scanner scanLine = new Scanner(scanner.nextLine());
 			for (int j = 0; j < board[i].length; j++) {
-				//needs to populate the array with random letters from random words in the list		
-				board[i][j] = allWordList.nextLetter(); 
+				// Only if you already have a board
+				String typedChar = "";
+
+				// try {
+				if (scanLine.hasNext()) {
+					typedChar = scanLine.next();
+				}
+
+				typedChar = typedChar.toUpperCase();
+				board[i][j] = typedChar.charAt(0);
+
+				// Only use if you want random letters
+				// board[i][j] = allWordList.nextLetter();
+			}
+			scanLine.close();
+		}
+		scanner.close();
+
+	}
+
+	public ArrayList<String> find() {
+		ArrayList<String> foundWords = new ArrayList<String>();
+		boolean visited[][] = new boolean[board.length][board[0].length];
+
+		// start from every tile on the board
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				find(board, visited, i, j, "", foundWords);
 			}
 		}
-	}
-	
-	public ArrayList<String> find() {
-		ArrayList<String> foundWords = new ArrayList<String>(); 	
-		boolean visited[][] = new boolean[board.length][board[0].length];
-        
-		//start from every tile on the board 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                find(board, visited, i, j, "", foundWords);
-            }
-        }
-        
-        //foundWords was passed as a reference to find()
-        //so it's value was changed here as well
-        return foundWords;
-        
-    }
-	
-	private void find(char board[][], boolean visited[][], 
-			int i, int j, String currentWord, ArrayList<String> foundWords) {
 
-		//change the current word based on the current letter
+		// foundWords was passed as a reference to find()
+		// so it's value was changed here as well
+		return foundWords;
+
+	}
+
+	private void find(char board[][], boolean visited[][], int i, int j, String currentWord,
+			ArrayList<String> foundWords) {
+
+		// change the current word based on the current letter
 		currentWord += board[i][j];
 
-		//base case is checking if word is longer or the board than the longest word
-		if(currentWord.length() > allWordList.getLongestWordLength()) {
+		// base case is checking if word is longer or the board than the longest word
+		if (currentWord.length() > allWordList.getLongestWordLength()) {
 			return;
 		}
-		
-	
-		//sets the current square as visited
+
+		// sets the current square as visited
 		visited[i][j] = true;
-		
-		//goes through each of the surrounding squares of the current square
-		//we also make sure we don't walk off the board
+
+		// goes through each of the surrounding squares of the current square
+		// we also make sure we don't walk off the board
 		for (int row = i - 1; row <= i + 1 && row < board.length; row++) {
 			for (int col = j - 1; col <= j + 1 && col < board.length; col++) {
-				//make sure we don't repeat letters
-				//also make sure we don't go off the board when we did -1
+				// make sure we don't repeat letters
+				// also make sure we don't go off the board when we did -1
 				if (row >= 0 && col >= 0 && !visited[row][col]) {
 					find(board, visited, row, col, currentWord, foundWords);
 				}
-				
+
 			}
 		}
-		
-		//checks if it is a valid word and if we already added the word
-		if(allWordList.contains(currentWord) && !foundWords.contains(currentWord)) {
-			//add the word to the found words
+
+		// checks if it is a valid word and if we already added the word
+		if (allWordList.contains(currentWord) && !foundWords.contains(currentWord)) {
+			// add the word to the found words
 			foundWords.add(currentWord);
 		}
-		//remove the bread crumbs
+		// remove the bread crumbs
 		visited[i][j] = false;
 	}
-	
-	//toString returns the board as a string
+
+	// toString returns the board as a string
 	public String toString() {
 		String result = "";
-		//goes through each tile
+		// goes through each tile
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
-				//adds the letter and a space
+				// adds the letter and a space
 				result += board[i][j] + "  ";
 			}
-			//add a line break for the next row
+			// add a line break for the next row
 			result += "\n";
 		}
-		//return the built string
+		// return the built string
 		return result;
 	}
 }
--- Boggle/src/Program.java
+++ Boggle/src/Program.java
@@ -1,72 +1,73 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
-	    double startTime = System.currentTimeMillis();
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to Boggle, by Ansh Verma. \n");
-		
-		
-		int size = 5;
+
+		System.out.println("What is the size of the board?");
+		int size = scanner.nextInt();
+		scanner.close();
 		WordList list = new WordList("WordList.txt", 3, 8);
 		Board board = new Board(list, size);
-		//add a line breaks
+		double startTime = System.currentTimeMillis();
+		// add a line breaks
 		System.out.println("\n");
-		//uses the board's toString()
+		// uses the board's toString()
 		System.out.println(board);
-		//calls my recursive method
+		// calls my recursive method
 		ArrayList<String> foundWords = board.find();
-		
-		//sorts the returned ArrayList using my WordComparator
+
+		// sorts the returned ArrayList using my WordComparator
 		Collections.sort(foundWords, new WordComparator());
-		
-		
-		//shows amount of words
+
+		// shows amount of words
 		System.out.println("Found " + (foundWords.size()) + " word(s) \n");
-	
-		//don't print any words if no words were found
-		if(foundWords.size() != 0) {
+
+		// don't print any words if no words were found
+		if (foundWords.size() != 0) {
 			int index = 0;
-			//start at the largest word size
-			//since we already sorted, the largest is the first word
+			// start at the largest word size
+			// since we already sorted, the largest is the first word
 			int wordSize = foundWords.get(index).length();
-			
-			
-			//we don't want to walk off the edge of the array
-			while(index < foundWords.size()) {
-				//we don't want to walk off the edge while looping
-				//we also don't want to put words with smaller lengths 
-				//in the wrong category
-				while(index < foundWords.size() && foundWords.get(index).length() == wordSize) {
-					
-					//we have the "index==0" so that we don't get an Exception.
-					//when doing "index-1" which would be -1, 
-					//which would not be an index on the array
-					
-					//the second part of the if statement checks if the 
-					//word before the current word was a different length.
-					//if it was, we reprint the new wordSize
-					if(index == 0 || foundWords.get(index - 1).length() != wordSize) {
+
+			// we don't want to walk off the edge of the array
+			while (index < foundWords.size()) {
+				// we don't want to walk off the edge while looping
+				// we also don't want to put words with smaller lengths
+				// in the wrong category
+				while (index < foundWords.size() && foundWords.get(index).length() == wordSize) {
+
+					// we have the "index==0" so that we don't get an Exception.
+					// when doing "index-1" which would be -1,
+					// which would not be an index on the array
+
+					// the second part of the if statement checks if the
+					// word before the current word was a different length.
+					// if it was, we reprint the new wordSize
+					if (index == 0 || foundWords.get(index - 1).length() != wordSize) {
 						System.out.println("\n" + wordSize + " letter words");
 					}
-					
-					//print the next word in the list
+
+					// print the next word in the list
 					System.out.println(foundWords.get(index));
-					//move index for our next word
+					// move index for our next word
 					index++;
 				}
-				//the size of words has changed, so we lower the wordSize
+				// the size of words has changed, so we lower the wordSize
 				wordSize--;
 			}
 
 		}
-        
-		//used to check the time taken
-	    double endTime = System.currentTimeMillis();
-	    double timeTaken = (endTime - startTime) / 1000;
-	    System.out.println("\nTime taken: " + timeTaken + " seconds");
+
+		// used to check the time taken
+		double endTime = System.currentTimeMillis();
+		double timeTaken = (endTime - startTime) / 1000;
+		System.out.println("\nTime taken: " + timeTaken + " seconds");
 	}
 
 }
